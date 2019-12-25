25d7f452eb6d4397c2f8adbd9be243ebc6e58bca
Hitesh Kumar <hitesh@singsys.com>
2019-12-25 17:53:06 +0530

    remove unnecessary data

--- src/components/Select.jsx
+++ src/components/Select.jsx
@@ -58,7 +58,7 @@ export default DirectSelect.extend({
         },
 
         async fetchDataFromServer(search, loading) {
-            if (!this.actiion) return false
+            if (!this.action) return false
             loading(true)
             await DirectSelect.options.methods.fetchDataFromServer.call(this, search)
             loading(false)
@@ -68,68 +68,5 @@ export default DirectSelect.extend({
             this.$emit('option:created', newOption)
             return newOption
         }
-        // /**
-        //  * To Render slots.
-        //  * @param {function} createElement 
-        //  * @param {Object} slots 
-        //  */
-        // renderSlots(createElement, slots) {
-        //     return this.groupBy ? this.renderOptionsWithGroup() : this.renderOptions(this.finalItems)
-        // },
-        // renderOptions(items) {
-        //     return items.map(item => {
-        //         const _val = this.$helper.isObject(item) ? item[this.itemValue] : item
-        //         const _disabled = this.$helper.isObject(item) ? !!item.disabled : false
-        //         const __disabled = _disabled || (this.disabledValues && this.disabledValues.includes(_val))
-        //         return this.$scopedSlots.item ? this.$scopedSlots.item({item, disabled: __disabled}) :
-        //             this.$createElement('el-option', {
-        //                 props: {
-        //                     key: `${this.id}_item_${item[this.itemValue]}`,
-        //                     label: this.$helper.isObject(item) ? item[this.itemText] : item,
-        //                     value: _val,
-        //                     disabled: __disabled
-        //                 }
-        //             })
-        //     })
-        // },
-        // renderOptionsWithGroup() {
-        //     return this.groups.map(group => {
-        //         const items = this.groupedOptions[group]
-        //         return this.$scopedSlots.groupItem ? this.$scopedSlots.groupItem({group, items}) : 
-        //             this.$createElement('el-option-group', {
-        //                 props: {
-        //                     key: group,
-        //                     label: group
-        //                 }
-        //             }, this.renderOptions(items))
-        //     })
-        // },
-        /**
-        * When value change internally.
-        * @param {any} value 
-        */
-        // onInput(value) {
-        //     if (this.isNotSame(value, this.LQElement)) {
-        //         const _values = value ? (this.$helper.isArray(value) ? value : [value]) : []
-        //         let items = [];
-        //         _values.forEach((val) => {
-        //             let _selected_item = val
-        //             this.items.every(_item => {
-        //                 if (_item[this.itemValue] === val) {
-        //                     _selected_item = _item
-        //                     return false
-        //                 }
-        //                 return true
-        //             })
-        //             items.push(_selected_item)
-        //         })
-        //         this.setValue((items[0] !== undefined ? (this.multiple ? items : items[0]) : value), true, true)
-        //     }
-        // },
-        // _extractOnlyValue() {
-        //     const _values = this.LQElement ? (this.$helper.isArray(this.LQElement) ? this.LQElement : [this.LQElement]) : []
-        //     const items = _values.map(t => typeof t === 'object' ? t[this.itemValue] : t)
-        //     return (items[0] !== undefined ? (this.multiple ? items : items[0]) : null)
-        // }
     }
 })
